c479f7fcf4f12e20bda143781e9d197d9f77bb7d
andrzejsydor <andrzejsydor@users.noreply.github.com>
2019-04-13 17:44:59 +0200

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,12 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', function(req,res) {
   res.send('Hello World!')
 })
 
-app.listen(8081, function() {
-  console.log('app listening on port 8081!')
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
 })
 
